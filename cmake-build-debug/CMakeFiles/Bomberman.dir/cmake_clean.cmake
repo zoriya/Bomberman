fb0da85416974d103d37fba5f32f2a3ba347f3cb
Zoe Roux <zoe.roux@sdg.moe>
2021-05-12 16:06:21 +0200

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/Bomberman.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "Bomberman"
+  "Bomberman.pdb"
+  "CMakeFiles/Bomberman.dir/sources/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Bomberman.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
