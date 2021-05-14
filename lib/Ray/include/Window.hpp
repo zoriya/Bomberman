f48890e882074b3a3def9af21935e51f38539519
arthur.jamet <arthur.jamet@epitech.eu>
2021-05-14 16:08:49 +0200

    window: fix indent

--- lib/Ray/include/Window.hpp
+++ lib/Ray/include/Window.hpp
@@ -15,68 +15,68 @@
 #include "Surface.hpp"
 
 namespace Ray {
-class Window: public Surface {
-    public:
-        // Creates window, and opens it if openNow is set to true
-        Window(int width, int height, const std::string title, bool openNow = false);
-        
-        //Closes window if still open
-        ~Window() = default;
+    class Window: public Surface {
+        public:
+            // Creates window, and opens it if openNow is set to true
+            Window(int width, int height, const std::string title, bool openNow = false);
+            
+            //Closes window if still open
+            ~Window() = default;
 
-        // Initialize window and OpenGL context
-        bool open(void);
+            // Initialize window and OpenGL context
+            bool open(void);
 
-        // Check if KEY_ESCAPE pressed or Close icon pressed
-        bool shouldClose(void) const;
+            // Check if KEY_ESCAPE pressed or Close icon pressed
+            bool shouldClose(void) const;
 
-        //Set key used to close window (default: ESC)
-        void setExitKey(Keyboard::Key key);
+            //Set key used to close window (default: ESC)
+            void setExitKey(Keyboard::Key key);
 
-        // Close window and unload OpenGL context
-        bool close(void);
+            // Close window and unload OpenGL context
+            bool close(void);
 
-        // Check if window is currently focused
-        bool isFocused(void);
+            // Check if window is currently focused
+            bool isFocused(void);
 
-        // Set window dimensions
-        const Ray::Vector2 &getDimensions(void) const;
+            // Set window dimensions
+            const Ray::Vector2 &getDimensions(void) const;
 
-        // Shows cursor
-        void showCursor(void);
+            // Shows cursor
+            void showCursor(void);
 
-        // Hides cursor
-        void hideCursor(void);
+            // Hides cursor
+            void hideCursor(void);
 
-        // Check if cursor is not visible
-        bool cursorIsHidden(void) const;
+            // Check if cursor is not visible
+            bool cursorIsHidden(void) const;
 
-        // Check if cursor is on the current screen.
-        bool cursorIsOnScreen(void) const;
+            // Check if cursor is on the current screen.
+            bool cursorIsOnScreen(void) const;
 
-        // Set target FPS (maximum)
-        void setFPS(int fps); 
+            // Set target FPS (maximum)
+            void setFPS(int fps); 
 
-        // Set background color (framebuffer clear color)
-        void clear(const Color &color);
+            // Set background color (framebuffer clear color)
+            void clear(const Color &color);
 
-        // Setup canvas (framebuffer) to start drawing
-        // Must be called before first draw of iteration
-        void beginDrawing(void);
+            // Setup canvas (framebuffer) to start drawing
+            // Must be called before first draw of iteration
+            void beginDrawing(void);
 
-        // End canvas drawing and swap buffers (double buffering)
-        // Must be called after last draw of iteration
-        void endDrawing(void);
+            // End canvas drawing and swap buffers (double buffering)
+            // Must be called after last draw of iteration
+            void endDrawing(void);
 
-        void draw(const Rectangle &);
-        void draw(const Line &);
-        void draw(const Point &);
-        void draw(const Circle &);
+            void draw(const Rectangle &);
+            void draw(const Line &);
+            void draw(const Point &);
+            void draw(const Circle &);
 
-    private:
-        Ray::Vector2 _dimensions;
-        std::string _title;
-        bool _isOpen;
-};
+        private:
+            Ray::Vector2 _dimensions;
+            std::string _title;
+            bool _isOpen;
+    };
 }
 
 #endif /* !WINDOW_HPP_ */
