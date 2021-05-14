5e26cb471612418a2b3af9d99744a61c01c13ef8
arthur.jamet <arthur.jamet@epitech.eu>
2021-05-14 19:13:12 +0200

    documentation for gamepad

--- lib/Ray/include/Controllers/Gamepad.hpp
+++ lib/Ray/include/Controllers/Gamepad.hpp
@@ -13,25 +13,47 @@
 #include <vector>
 
 namespace Ray {
+
+    //! @brief Entity representing a gamepad controller
     class GamePad {
         public:
             typedef ::GamepadButton Button;
 
+            //! @brief A default constructor
+            //! @param The id of the controller 
             GamePad(int id);
 
+            //! @brief A default destructor
             ~GamePad() = default;
 
+            //! @brief A default copy constructor
             GamePad(const GamePad &) = default;
 
+            //! @brief An entity is assignable
+            //! @return A reference to the assigned object
             GamePad &operator=(const GamePad &) = default;
 
+            //! @brief Returns true if Button is pressed on the gamepad
+            //! @param Button The keycode of the button
             bool isPressed(Button);
+
+            //! @brief Returns true if Button is down on the gamepad
+            //! @param Button The keycode of the button
             bool isDown(Button);
+
+            //! @brief Returns true if Button is released on the gamepad
+            //! @param Button The keycode of the button
             bool isReleased(Button);
+
+            //! @brief Returns true if Button is up on the gamepad
+            //! @param Button The keycode of the button
             bool isUp(Button);
+
+            //! @brief Returns a vector containing keycode of currently pressed keys
             std::vector<GamePad::Button> getPressedButtons(void);
         
         private:
+            //! @brief The id of the controller, used to fetch buttons' states 
             int _id;
     };
 }
