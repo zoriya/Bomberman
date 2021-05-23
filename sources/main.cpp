d1c3dc6e2572b856daa3f9c2f0e1261f5b7a2240
Clément Le Bihan <clement.lebihan773@gmail.com>
2021-05-23 16:49:01 +0200

    fixing compil

--- sources/main.cpp
+++ sources/main.cpp
@@ -7,19 +7,8 @@
 
 
 #include <iostream>
-<<<<<<< Updated upstream
-#include <Wal.hpp>
-=======
 #include <cmath>
-#include "Window.hpp"
-#include "Drawables/2D/Text.hpp"
-#include "Drawables/2D/Circle.hpp"
-#include "Controllers/Keyboard.hpp"
-#include "Camera/Camera3D.hpp"
-#include "Drawables/3D/Grid.hpp"
-#include "Drawables/3D/Cube.hpp"
 #include "Wal.hpp"
->>>>>>> Stashed changes
 
 int main()
 {
