c1c832b803ad39c2f67135e667dc816b7d291862
Bluub <louis.auzuret@gmail.com>
2021-05-24 15:11:53 +0200

    clickable start, need to merge

---
+++ sources/Component/Clickable/ClickableComponent.hpp
@@ -0,0 +1,17 @@
+//
+// Created by Louis Auzuret on 2021-05-24
+//
+
+#pragma once
+
+#include "lib/wal/sources/Component/Component.hpp"
+
+namespace BBM
+{
+	class ClickableComponent : public WAL::Component
+	{
+		private:
+		protected:
+		public:
+	};
+} //namespace BBM
