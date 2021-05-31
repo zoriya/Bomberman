44b68b15e643690f32de96cab3f91dcd89988f15
Zoe Roux <zoe.roux@sdg.moe>
2021-05-31 17:19:23 +0200

    Adding a timer component

---
+++ sources/Component/Timer/TimerComponent.cpp
@@ -0,0 +1,32 @@
+//
+// Created by Zoe Roux on 5/31/21.
+//
+
+#include "TimerComponent.hpp"
+
+#include <utility>
+
+namespace BBM
+{
+	TimerComponent::TimerComponent(WAL::Entity &entity, std::chrono::nanoseconds delay)
+		: WAL::Component(entity),
+		ringIn(delay)
+	{}
+
+	TimerComponent::TimerComponent(WAL::Entity &entity, std::chrono::nanoseconds delay, const WAL::Callback<WAL::Entity &> &callback)
+		: WAL::Component(entity),
+		ringIn(delay),
+		callback(callback)
+	{}
+
+	TimerComponent::TimerComponent(WAL::Entity &entity, std::chrono::nanoseconds delay, std::function<void(WAL::Entity &)> callback)
+		: WAL::Component(entity),
+		ringIn(delay),
+		callback(std::move(callback))
+	{}
+
+	WAL::Component *TimerComponent::clone(WAL::Entity &entity) const
+	{
+		return new TimerComponent(entity, this->ringIn, this->callback);
+	}
+}
---
+++ sources/Component/Timer/TimerComponent.hpp
@@ -0,0 +1,37 @@
+//
+// Created by Zoe Roux on 5/31/21.
+//
+
+#pragma once
+
+#include <Component/Component.hpp>
+#include <chrono>
+#include "Models/Callback.hpp"
+
+namespace BBM
+{
+	//! @brief
+	class TimerComponent : public WAL::Component
+	{
+	public:
+		//! @brief The callback to call when the timer ring.
+		WAL::Callback<WAL::Entity &> callback;
+		//! @brief The ring delay of this timer component.
+		std::chrono::nanoseconds ringIn;
+
+		Component * clone(WAL::Entity &entity) const override;
+
+		//! @brief A default constructor
+		TimerComponent(WAL::Entity &entity, std::chrono::nanoseconds delay);
+		//! @brief Create a timer with a callback.
+		TimerComponent(WAL::Entity &entity, std::chrono::nanoseconds delay, const WAL::Callback<WAL::Entity &> &callback);
+		//! @brief Create a timer with a function to call on ring.
+		TimerComponent(WAL::Entity &entity, std::chrono::nanoseconds delay, std::function<void (WAL::Entity &)> callback);
+		//! @brief A timer component is copy constructable
+		TimerComponent(const TimerComponent &) = default;
+		//! @brief A default destructor
+		~TimerComponent() override = default;
+		//! @brief A component is not assignable.
+		TimerComponent &operator=(const TimerComponent &) = delete;
+	};
+}
