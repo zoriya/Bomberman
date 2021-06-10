54f29444325c56915aa5ead386b0acfc5177f7cc
Clément Le Bihan <clement.lebihan773@gmail.com>
2021-06-10 10:58:12 +0200

    fixing indentation in KeyboardComponent.hpp

--- sources/Component/Keyboard/KeyboardComponent.hpp
+++ sources/Component/Keyboard/KeyboardComponent.hpp
@@ -15,42 +15,42 @@ namespace BBM
 {
 	class KeyboardComponent : public WAL::Component
 	{
-		public:
-			//! @brief jump key
-			Key keyJump = KEY_SPACE;
-			//! @brief bomb key
-			Key keyBomb = KEY_E;
-			//! @brief pause key
-			Key keyPause = RAY::Controller::Keyboard::Key::KEY_ESCAPE;
-			//! @brief move right key
-			Key keyRight = KEY_D;
-			//! @brief move left key
-			Key keyLeft = KEY_A;
-			//! @brief move up key
-			Key keyUp = KEY_W;
-			//! @brief move down key
-			Key keyDown = KEY_S;
+	public:
+		//! @brief jump key
+		Key keyJump = KEY_SPACE;
+		//! @brief bomb key
+		Key keyBomb = KEY_E;
+		//! @brief pause key
+		Key keyPause = RAY::Controller::Keyboard::Key::KEY_ESCAPE;
+		//! @brief move right key
+		Key keyRight = KEY_D;
+		//! @brief move left key
+		Key keyLeft = KEY_A;
+		//! @brief move up key
+		Key keyUp = KEY_W;
+		//! @brief move down key
+		Key keyDown = KEY_S;
 
-			//! @inherit
-			WAL::Component *clone(WAL::Entity &entity) const override;
-		
-			//! @brief Create a new keyboard component using custom keys.
-			KeyboardComponent(WAL::Entity &entity,
-							  Key up = KEY_W,
-							  Key down = KEY_S,
-							  Key left = KEY_A,
-							  Key right = KEY_D,
-							  Key jump = KEY_SPACE,
-							  Key bomb = KEY_E,
-							  Key pause = RAY::Controller::Keyboard::Key::KEY_ESCAPE);
+		//! @inherit
+		WAL::Component *clone(WAL::Entity &entity) const override;
 
-			//! @brief A Keyboard component is copy constructable.
-			KeyboardComponent(const KeyboardComponent &) = default;
+		//! @brief Create a new keyboard component using custom keys.
+		KeyboardComponent(WAL::Entity &entity,
+		                  Key up = KEY_W,
+		                  Key down = KEY_S,
+		                  Key left = KEY_A,
+		                  Key right = KEY_D,
+		                  Key jump = KEY_SPACE,
+		                  Key bomb = KEY_E,
+		                  Key pause = RAY::Controller::Keyboard::Key::KEY_ESCAPE);
 
-			//! @brief default destructor
-			~KeyboardComponent() override = default;
+		//! @brief A Keyboard component is copy constructable.
+		KeyboardComponent(const KeyboardComponent &) = default;
 
-			//! @brief A Keyboard component can't be assigned
-			KeyboardComponent &operator=(const KeyboardComponent &) = delete;
+		//! @brief default destructor
+		~KeyboardComponent() override = default;
+
+		//! @brief A Keyboard component can't be assigned
+		KeyboardComponent &operator=(const KeyboardComponent &) = delete;
 	};
 }
