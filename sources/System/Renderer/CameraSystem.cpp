8d3010ecd04710d3d4f89fc5e4905ea763a2670f
Askou <tom.augier@epitech.eu>
2021-06-18 15:32:35 +0200

    Remove useless add

--- sources/System/Renderer/CameraSystem.cpp
+++ sources/System/Renderer/CameraSystem.cpp
@@ -60,9 +60,6 @@ namespace BBM
 		float lowerZDist = 0;
 
 		for (auto &[player, position, _] : this->_wal.getScene()->view<PositionComponent, TagComponent<Player>>()) {
-			// To be remove !
-			if (!player.hasComponent<MovableComponent>())
-				player.addComponent<MovableComponent>();
 			playerPos.emplace_back(position.position);
 		}
 		if (playerPos.size() == 1)
